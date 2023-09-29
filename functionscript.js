d67a4a3d3b951374e8efb67012b1e545d27b2c84
sumit <sumitmessi018@gmail.com>
2023-09-30 01:28:20 +0530

    seventh

---
+++ functionscript.js
@@ -0,0 +1,19 @@
+function onePlusAvg(x,y){
+    console.log("done")
+    return 1 + (x + y)/2
+    }
+    
+    
+    
+    
+    
+    
+    let a = 1;
+    let b = 2;
+    let c = 3;
+    
+    console.log("one plus average of a and b is ", 1+ (a + b)/2)
+    console.log("one plus average of b and c is ", 1 + (b + c)/2)
+    console.log("one plus average of a and c is ", 1 +(a+ c)/2)
+     
+    
